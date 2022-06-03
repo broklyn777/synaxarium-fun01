682af60acffad518d3dbbce7bba02943be14243c
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-06-03 19:56:38 +0200

    change cards

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,6 @@
 
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
 import CategoryLabel from './CategoryLabel'
 
 export default function Post({ post, compact }) {
@@ -16,7 +16,7 @@ export default function Post({ post, compact }) {
           <Image
             src={post.frontmatter.thumbnailUrl}
             alt=''
-            height={400}
+            height={250}
             width={500}
             className='mb-4 rounded'
             objectFit="cover"
@@ -33,30 +33,26 @@ export default function Post({ post, compact }) {
 
         <div className='mt-2'>
 
-          <a className='text-2xl text-gray-700 font-bold hover:underline decoration-primary-500'>
+          <a className='text-lg text-gray-700 font-semibold hover:underline decoration-primary-500'>
             {post.frontmatter.title}
           </a>
 
-          <p className='mt-2 text-gray-600'>{post.frontmatter.description}</p>
+          <p className=' leading-[18px] font-thin text-gray-600'>{post.frontmatter.description}</p>
         </div>
 
-        {!compact && (
+        {/* {!compact && (
           <div className='flex justify-between items-center mt-6'>
 
 
 
             <div className='flex items-center'>
-              {/* <Image
-              src={post.frontmatter.author_image}
-              alt=''
-              className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'
-            /> */}
+
               <h3 className='text-gray-700 '>
                 {post.frontmatter.author}
               </h3>
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </Link>
 
